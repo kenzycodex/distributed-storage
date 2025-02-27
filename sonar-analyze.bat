d66fb756d6c5039257185f4e293e203a4489c15f
kenzycodex <kenzycodex@gmail.com>
2025-02-27 18:59:15 +0100

    Fix SonarCloud issues: Replace wildcards with specific types and extract string literals as constants

--- sonar-analyze.bat
+++ sonar-analyze.bat
@@ -3,4 +3,4 @@ REM Load environment variables from .env file
 for /F "tokens=*" %%A in (.env) do set %%A
 
 REM Run SonarCloud analysis with explicit parameters
-mvn clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=%SONAR_ORGANIZATION% -Dsonar.projectKey=%SONAR_PROJECT_KEY% -Dsonar.login=%SONAR_TOKEN%
+mvn clean verify sonar:sonar -Dsonar.host.url=https://sonarcloud.io -Dsonar.organization=%SONAR_ORGANIZATION% -Dsonar.projectKey=%SONAR_PROJECT_KEY% -Dsonar.token=%SONAR_TOKEN%
